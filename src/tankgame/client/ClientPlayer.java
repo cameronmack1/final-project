a697d550e0c9ddf3d5fdf44ec2c60bb74a5f3f46
cameronmack1 <ckmackenzie31@gmail.com>
2026-04-02 12:44:20 -0400

    singleplayer debug

--- src/tankgame/client/ClientPlayer.java
+++ src/tankgame/client/ClientPlayer.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package tankgame.game;
+package tankgame.client;
 
 /**
  *
- * @author Cameron
+ * @author Layne
  */
 public class ClientPlayer {
     
