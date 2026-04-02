7afc5a3347aa1e08a79cb48cf3ebd9bd282d9b99
LayneRipley <Layne.Ripley@gmail.com>
2026-04-02 13:21:14 -0400

    e

--- src/tankgame/game/Player.java
+++ src/tankgame/game/Player.java
@@ -4,5 +4,15 @@ package tankgame.game;
  * @author Layne
  */
 public abstract class Player {
+   private double x = 0;
+   private double y = 0;
+   private double angle;
     
+  public double getX(){
+      return x;
+  }
+  public double getY(){
+      return y;
+  }
 }
+
