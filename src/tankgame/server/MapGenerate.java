2b917320d41a21b3200a4f7fae87011668c4f0c8
CodingMan273 <colinpheron@gmail.com>
2026-04-07 13:16:27 -0400

    hello my chuds

--- src/tankgame/server/MapGenerate.java
+++ src/tankgame/server/MapGenerate.java
@@ -1,8 +1,17 @@
 package tankgame.server;
+
 /**
  *
  * @author Colin
  */
 public class MapGenerate {
-    
+    //bouta make some maze generation
+
+    boolean generateMap = true;
+
+    public void mapGeneration() {
+        if (generateMap) {
+            System.out.println("generated ts");
+        }
+    }
 }
