cbce659a4563cc71ef5f697613a56dae1ae4cd20
cameronmack1 <ckmackenzie31@gmail.com>
2026-04-07 12:54:35 -0400

    skibidi

--- src/tankgame/game/GameScreen.java
+++ src/tankgame/game/GameScreen.java
@@ -20,12 +20,14 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import tankgame.client.ClientPlayer;
+
 /**
  *
  * @author Cameron
  */
 public final class GameScreen extends JFrame {
 
+    KeyHandler kb = new KeyHandler();
     ArrayList<Player> playerList;
     int gameState;
     boolean isHost;
@@ -46,9 +48,8 @@ public final class GameScreen extends JFrame {
         g2d = img.createGraphics();
         getContentPane().add(new JLabel(new ImageIcon(img)));
         setVisible(true);
-        KeyHandler kb = new KeyHandler();
         addKeyListener(kb);
-        
+
         gameState = 10;
         this.initDebug();
 
@@ -74,18 +75,20 @@ public final class GameScreen extends JFrame {
     public void tick() {
         g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, width, height);
-
-        gameState = 1;
         switch (gameState) {
             case 0: {//in menu
 
                 break;
             }
-            case 1: {//in game
+            case 1: {//client in game
                 g2d.setColor(Color.WHITE);
                 for (ServerPlayer player : playerHandler.getPlayers()) {
                     g2d.fillOval(player.x, player.y, 10, 10);
                 }
+                break;
+            }
+            case 2: {//server in game
+                
                 break;
             }
 
