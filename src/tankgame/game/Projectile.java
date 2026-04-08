82685d6643e5d140fa20f99fec19fdcf140cb29f
LayneRipley <Layne.Ripley@gmail.com>
2026-04-08 12:51:03 -0400

    added projectile class

---
+++ src/tankgame/game/Projectile.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tankgame.game;
+
+/**
+ *
+ * @author layne
+ */
+public class Projectile {
+    protected double x;
+    protected double y;
+    double angle;
+    public static double velocity;
+}
