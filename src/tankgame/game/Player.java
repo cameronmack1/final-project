3815cf97079d812767c6bac013a9f3cb7eb915ee
LayneRipley <Layne.Ripley@gmail.com>
2026-04-09 13:21:37 -0400

    added velocity and tank rotation (buggy)

--- src/tankgame/game/Player.java
+++ src/tankgame/game/Player.java
@@ -1,39 +1,53 @@
 package tankgame.game;
+
 /**
  *
  * @author Layne
  */
 public abstract class Player {
-   private double x = 0;
-   private double y = 0;
-   private double angle;
-    
-  public double getX(){
-      return x;
-  }
-  public double getY(){
-      return y;
-  }
-  
-  public double getAngle(){
-      return angle;
-  }
-  
-  public void move(boolean[] arr){ //up[0], left[1], down[2], right[3] message by Layne Ripley
-  
-      if (arr[0] && !arr[2]){
-          y -= 5;
-      }
-       if (arr[2] && !arr[0]){
-          y += 5;
-      }
-       if (arr[1] && !arr[3]){
-          x -= 5;
-      }
-        if (arr[3] && !arr[1]){
-          x += 5;
-      }
-      
-  }   
+
+    private double x = 0;
+    private double y = 0;
+    private double angle;
+    public double acceleration = 0;
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void move(boolean[] keys) { //up[0], left[1], down[2], right[3] message by Layne Ripley
+
+        if (keys[0] ^ keys[2]) {//if only one of forwards or backwards pressed
+            if (acceleration > 0 && keys[1]) {
+                angle += 0.1;
+                acceleration += 0.1;
+            }
+            if (keys[0] && keys[3]) {
+                angle -= 0.1;
+                acceleration += 0.1;
+            }
+            if(keys[2] && keys[1]){
+              angle += 0.1;
+              acceleration -= 0.1;
+          }
+            if(keys[2] && keys[3]){
+              angle -= 0.1;
+              acceleration -= 0.1;
+          }
+     
+        }
+        y += Math.sin(angle) * acceleration;
+        x += Math.cos(angle) * acceleration;
+        
+    }
+
 }
 
