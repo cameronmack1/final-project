4f0d98e8b74d085ab4fd4a76aa52e68d6d22a3ce
LayneRipley <Layne.Ripley@gmail.com>
2026-04-09 12:36:33 -0400

    added more inputs for player movement

--- src/tankgame/game/KeyHandler.java
+++ src/tankgame/game/KeyHandler.java
@@ -17,18 +17,22 @@ public class KeyHandler extends KeyAdapter {
     @Override
     public void keyPressed(KeyEvent e){
         switch(e.getKeyCode()){
+            case KeyEvent.VK_UP:
             case KeyEvent.VK_W:{
                 keys[0] = true;
                 break;
             }
+            case KeyEvent.VK_LEFT:
             case KeyEvent.VK_A:{
                 keys[1] = true;
                 break;
             }
+            case KeyEvent.VK_DOWN:
             case KeyEvent.VK_S:{
                 keys[2] = true;
                 break;
             }
+            case KeyEvent.VK_RIGHT:
             case KeyEvent.VK_D:{
                 keys[3] = true;
                 break;
@@ -43,18 +47,23 @@ public class KeyHandler extends KeyAdapter {
     @Override
     public void keyReleased(KeyEvent e){
         switch(e.getKeyCode()){
+            
+            case KeyEvent.VK_UP:
             case KeyEvent.VK_W:{
                 keys[0] = false;
                 break;
             }
+            case KeyEvent.VK_LEFT:
             case KeyEvent.VK_A:{
                 keys[1] = false;
                 break;
             }
+            case KeyEvent.VK_DOWN:
             case KeyEvent.VK_S:{
                 keys[2] = false;
                 break;
             }
+            case KeyEvent.VK_RIGHT:
             case KeyEvent.VK_D:{
                 keys[3] = false;
                 break;
@@ -63,6 +72,8 @@ public class KeyHandler extends KeyAdapter {
                 keys[4] = false;
                 break;
             }
+            
+           
         }
     }
 }
