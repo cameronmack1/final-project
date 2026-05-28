1cf0bd38b875a7ba79ca48ee5fc257e67a0a6a14
LayneRipley <layne.ripley@gmail.com>
2026-05-27 20:03:08 -0400

    added logic to LobbyPlayer yoski and addPlayer method

--- src/tankgame/server/LobbyPlayer.java
+++ src/tankgame/server/LobbyPlayer.java
@@ -3,16 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tankgame.server;
-
+import java.util.UUID;
 /**
  *
  * @author layne
  */
 public class LobbyPlayer  {
     private String name;
-   public LobbyPlayer (String name){
+    private UUID ID;
+    
+   public LobbyPlayer (String name, UUID ID){
        
        this.name = name;
+       this.ID = ID;
    }
     
+ 
+   
 }
